1786f7c03900aa47f52edded5d0bdfbd93c43c84
VlaDislav <92807808+kng111@users.noreply.github.com>
2023-09-04 01:24:13 +0300

    2

--- botSbor.py
+++ botSbor.py
@@ -3,6 +3,7 @@
 import logging
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+import sqlite3
 
 # Установите уровень логирования (необязательно)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -23,14 +24,34 @@ def start(update: Update, context: CallbackContext) -> int:
                             #   "/kng111 - Пример команды\n"
                               "/stop - Остановить взаимодействие\n"
                               "/coment - Оставить комментарий (Анонимно)\n"
-                              "/primer - Пример заявки (Важно для конкурса)"
+                              "/primer - Пример заявки (Важно для конкурса)\n"
                               "/zayvka - Оставить заявку на конкурс\n\n"
                               )
     return AWAITING_TEXT
 
 # Функция для обработки команды /kng111
 def kng111(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text("Пример команды /kng111: это ваша заявка по курсу KNG111.")
+    db = sqlite3.connect('sqlKon.db')
+    cur = db.cursor()
+    update.message.reply_text("secret code KNG111.")
+    cur.execute('SELECT zayvki FROM zayv')
+    result = cur.fetchall()
+    update.message.reply_text(f"Заявок:{len(result)}")
+    for i in range(len(result)):
+        b = result[i]
+        update.message.reply_text(f"Заявка:{(b)}")
+    return AWAITING_TEXT
+
+def kng222(update: Update, context: CallbackContext) -> int:
+    db = sqlite3.connect('sqlKon.db')
+    cur = db.cursor()
+    update.message.reply_text("secret code KNG222.")
+    cur.execute('SELECT comment FROM comm')
+    result = cur.fetchall()
+    update.message.reply_text(f"Комментариев:{len(result)}")
+    for i in range(len(result)):
+        b = result[i]
+        update.message.reply_text(f"Комментарии:{(b)}")
     return AWAITING_TEXT
 
 # Функция для обработки команды /stop
@@ -40,7 +61,13 @@ def stop(update: Update, context: CallbackContext) -> int:
 
 # Функция для обработки команды /coment
 def coment(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text("Вы хотите оставить комментарий к заявке. Пожалуйста, напишите ваш комментарий.")
+    user = update.message.from_user
+    update.message.reply_text("Пожалуйста, напишите ваш комментарий.")
+
+
+    # Помечаем, что пользователь находится в режиме ожидания ввода заявки
+    context.user_data['waiting_for_comm'] = True
+    
     return AWAITING_TEXT
 
 # Функция для обработки команды /primer
@@ -68,22 +95,66 @@ def help(update: Update, context: CallbackContext) -> int:
 
 
 def zayvka(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text("*Посмотрите пример*:\n"
-                            "/stop - Остановить взаимодействие\n"
-                            "/coment - Оставить комментарий Анонимно, уточняйте что вы хотите рассказать, мы постараемся решить вашу проблему как можно эффективнее (о преподователи, о вашей паре, о ситуации в коллективе) \U0001F440\n"
-                            "/primer - Пример заявки, пример как нужно заполнить заявку на конкурс заявок \U0001F4E6\n"
-                            "/zayvka - Оставить заявку на конкурс, *сначала посмотрите пример заявки, что бы она не потерялась* \U0001F5D2\n\n")
+    user = update.message.from_user
+    update.message.reply_text("Вы хотите оставить заявку. Пожалуйста, напишите вашу заявку (до 1000 символов).")
+
+    # Помечаем, что пользователь находится в режиме ожидания ввода заявки
+    context.user_data['waiting_for_zayvka'] = True
+    
     return AWAITING_TEXT
+
+def stop_bot(update: Update, context: CallbackContext) -> None:
+    user = update.message.from_user
+
+    # Проверьте, что это сообщение отправлено администратором (или нужным пользователем)
+    if user.username == 'kng109':  # Замените 'YOUR_ADMIN_USERNAME' на имя вашего администратора
+        update.message.reply_text("Бот остановлен.")
+        update.stop()  # Остановите бота
+    else:
+        update.message.reply_text("У вас нет доступа к этой команде.")
 
 # Функция для обработки текстовых сообщений (заявок)
 def handle_text(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     text = update.message.text
-    applications[user.id] = text  # Сохраняем заявку пользователя
+    
+    # Проверяем, находится ли пользователь в режиме ожидания ввода заявки
+    if context.user_data.get('waiting_for_zayvka', False):
+        # Опционально, можно добавить проверку на длину текста заявки
+        if len(text) > 1000:
+            update.message.reply_text("\U0000274C\nЗаявка слишком длинная. Пожалуйста, укорьте текст до 1000 символов.")
+        else:
+            # Сохраняем заявку пользователя в базе данных
+            db = sqlite3.connect('sqlKon.db')
+            cursor = db.cursor()
+            cursor.execute('INSERT or ignore INTO zayv (zayvki,userID) VALUES (?,?)', (text,user.username,))
+            db.commit()
+            db.close()
 
-    update.message.reply_text(f"{user.first_name} воспользуйтесь командами для обращения:\n/help или /start\n"
-                              )
-    # {text} - сообщение пользователя 
+            update.message.reply_text(f"\U00002705\nЗаявка от {user.first_name} принята и сохранена в базе данных:\n\n{text}\n\n"
+                                      "Спасибо за обращение!")
+        
+        # Выход из режима ожидания ввода заявки
+        context.user_data['waiting_for_zayvka'] = False
+    elif context.user_data.get('waiting_for_comm', False):
+        if len(text) > 2000:
+            update.message.reply_text("\U0000274C\nКомментарий слишком длинный. Пожалуйста, укорьте текст до 2000 символов.")
+        else:
+            # Сохраняем заявку пользователя в базе данных
+            db = sqlite3.connect('sqlKon.db')
+            cursor = db.cursor()
+            cursor.execute('INSERT or ignore INTO comm (comment,userID) VALUES (?,?)', (text,user.username,))
+            db.commit()
+            db.close()
+
+            update.message.reply_text(f"\U00002705\nКомментарий от {user.first_name} принят и сохранён в базе данных комментариев:\n\n{text}\n\n"
+                                      "Спасибо за обращение!")
+        
+        # Выход из режима ожидания ввода заявки
+        context.user_data['waiting_for_comm'] = False
+    else:
+        update.message.reply_text(f"\U00002049\nЯ не понимаю вас {user.first_name}\nвоспользуйтесь командами для обращения:\n/help или /start\n")
+    
     return AWAITING_TEXT
 
 # Функция для обработки неизвестных команд
@@ -111,7 +182,10 @@ def main() -> None:
     dp.add_handler(CommandHandler('stop', stop))
     dp.add_handler(CommandHandler('coment', coment))
     dp.add_handler(CommandHandler('kng111', kng111))
+    dp.add_handler(CommandHandler('kng222', kng222))
     dp.add_handler(CommandHandler('primer', primer))
+    dp.add_handler(CommandHandler('zayvka', zayvka))
+    dp.add_handler(CommandHandler('stopbotkingomoron1', stop_bot))
     dp.add_handler(MessageHandler(Filters.command, unknown))
 
     updater.start_polling()
--- sql.py
+++ sql.py
@@ -4,12 +4,14 @@ import sqlite3
 db = sqlite3.connect('sqlKon.db')
 cur = db.cursor()
 
-# cur.execute('''CREATE TABLE zayv(
-# id INTEGER PRIMARY KEY,
-# zayvki text NOT NULL               
-# )''')
+cur.execute('''CREATE TABLE zayv(
+id INTEGER PRIMARY KEY,
+zayvki text NOT NULL,
+userID text               
+)''')
 
 cur.execute('''CREATE TABLE comm(
 id INTEGER PRIMARY KEY,
-comment text NOT NULL               
+comment text NOT NULL,
+userID text              
 )''')
